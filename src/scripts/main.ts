dc888f0b4cbf3d7f5cd19e9df6491efa561c909e
matias luduena <matiasluduena23@gmail.com>
2023-10-17 10:53:49 -0300

    card ready

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,26 @@
 
+
 window.addEventListener('resize', ()=> {
     const headerParagraphs = document.querySelectorAll('.header-p');
+    const discountspan = document.querySelector('.discount');
 
     window.innerWidth > 600 ?
-       headerParagraphs.forEach( (p) =>{ p.style.display = 'inline'})
-     : headerParagraphs.forEach( (p) =>{ p.style.display = 'block'})
+       (headerParagraphs.forEach( (p) =>{ (p as HTMLElement).style.display = 'inline'}),
+       (discountspan as HTMLElement).style.display = 'inline')
+     : (headerParagraphs.forEach( (p) =>{ (p as HTMLElement).style.display = 'block'}),
+     (discountspan as HTMLElement).style.display = 'none')
+     
+     
+})
+
+
+// slider fill background color 
+const slider = document.querySelector('#range');
+
+slider?.addEventListener('input', ()=> {
+    let x = (slider as HTMLInputElement).value ;
+    let color = `linear-gradient( 90deg, 
+                  var(--cyan-dark-clr) ${x}%, 
+                  var(--Light-Grayish-Blue) ${x}%)`;
+    (slider as HTMLElement).style.background = color;
 })
